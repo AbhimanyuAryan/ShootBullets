69cabfcabfc2d16321fa0b11c1d6b18f26bbcb4d
DESKTOP-2LV9GDO\GK_Desk_07 <abhimanyu.chat@gmail.com>
2017-12-14 08:18:27 +0530

    ClientRPC and Command

---
+++ Assets/FireControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class FireControl : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+	public GameObject bulletSpawn;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(!isLocalPlayer) return;
+
+		if(Input.GetKeyDown("space"))
+		{
+			CmdShoot(); 
+		}
+	}
+
+	[Command] void CmdShoot() 
+	{
+		GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+		//bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.forward * 2000);
+		bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward*50;
+		NetworkServer.Spawn(bullet);
+		Destroy(bullet, 3.0f); 
+	}
+
+	
+}
